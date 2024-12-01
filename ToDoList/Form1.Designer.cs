be47b6e716ab037ca9b7f61fe8ebdb2c30a7af64
Martin Vallejo <vallejomartin6@gmail.com>
2024-12-01 20:55:30 -0300

    Agregar archivos de proyecto.

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTareaNueva = new System.Windows.Forms.TextBox();
+            this.dataGridViewTareas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTareas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(424, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCrear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrear.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCrear.Location = new System.Drawing.Point(36, 221);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(168, 45);
+            this.btnCrear.TabIndex = 3;
+            this.btnCrear.Text = "Crear Tarea";
+            this.btnCrear.UseVisualStyleBackColor = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(36, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Borrar Tarea";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTareaNueva
+            // 
+            this.txtTareaNueva.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTareaNueva.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
+            this.txtTareaNueva.ForeColor = System.Drawing.Color.Crimson;
+            this.txtTareaNueva.Location = new System.Drawing.Point(12, 155);
+            this.txtTareaNueva.Name = "txtTareaNueva";
+            this.txtTareaNueva.Size = new System.Drawing.Size(215, 20);
+            this.txtTareaNueva.TabIndex = 9;
+            this.txtTareaNueva.Text = "Nuevo Recordatorio";
+            this.txtTareaNueva.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dataGridViewTareas
+            // 
+            this.dataGridViewTareas.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewTareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTareas.Location = new System.Drawing.Point(276, 98);
+            this.dataGridViewTareas.Name = "dataGridViewTareas";
+            this.dataGridViewTareas.Size = new System.Drawing.Size(490, 448);
+            this.dataGridViewTareas.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(778, 558);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewTareas);
+            this.Controls.Add(this.txtTareaNueva);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCrear);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To Do List";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTareas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTareaNueva;
+        private System.Windows.Forms.DataGridView dataGridViewTareas;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Drawing;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LimpiarTextBox();
+            dataGridViewTareas.Columns.Add("Tarea", "Lista de Tarea"); // Agregar columna para las                                         
+            dataGridViewTareas.Columns["Tarea"].HeaderCell.Style.Font = new Font("Century Gothic", 12, FontStyle.Bold); // Cambiar la fuente del título de la columna
+            dataGridViewTareas.Columns["Tarea"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter; // Alineo al cnetro
+            dataGridViewTareas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; // Ajustar el tamaño de la columna
+            CargarInformacion();
+           
+        }
+        
+        public class Tarea
+        {
+            public string Descripcion { get; set; }
+        }
+        
+        private void GuardarTareas(List<Tarea> tareas)
+        {
+            string json = JsonSerializer.Serialize(tareas);
+            File.WriteAllText("tareas.json", json);
+        }
+
+        public List<Tarea> CargarTareas()
+        {
+            if (File.Exists("tareas.json"))
+            {
+                string json = File.ReadAllText("tareas.json");
+                return JsonSerializer.Deserialize<List<Tarea>>(json);
+            }
+            return new List<Tarea>();
+        }
+
+        private void CargarInformacion()
+        {
+            List<Tarea> tareas = CargarTareas();
+
+            foreach (var tarea in tareas)
+            {
+                dataGridViewTareas.Rows.Add(tarea.Descripcion);  // Agregar la tarea al DataGridView
+            }
+        }
+
+        #region Limpiar Texto Nuevas Tareas
+        private void LimpiarTextBox()
+        {
+            txtTareaNueva.Text = "Nuevo Recordatorio";
+            /** Asignacion de eventos enter y leave**/
+            txtTareaNueva.Enter += TxtTareaNueva_Enter;
+            txtTareaNueva.Leave += TxtTareaNueva_Leave;
+
+        }
+
+        /** Limpia el texto de la caja**/
+        private void TxtTareaNueva_Enter(object sender, EventArgs e)
+        {
+            if(txtTareaNueva.Text == "Nuevo Recordatorio")
+            {
+                txtTareaNueva.Text = string.Empty; // Limpia el texto
+                txtTareaNueva.ForeColor = System.Drawing.Color.Black;
+                txtTareaNueva.Font = new System.Drawing.Font(txtTareaNueva.Font.FontFamily, 10);
+            }
+        }
+
+       /** Restaura el texto de la caja**/
+        private void TxtTareaNueva_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTareaNueva.Text))
+            {
+                txtTareaNueva.Text = "Nuevo Recordatorio";
+                txtTareaNueva.ForeColor = System.Drawing.Color.Crimson;
+                txtTareaNueva.Font = new System.Drawing.Font(txtTareaNueva.Font.FontFamily, 12);
+            }
+        }
+
+        #endregion
+
+        
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            if(txtTareaNueva.Text != "Nuevo Recordatorio")
+            {
+                
+                try
+                {
+                    // Verificar que el TextBox no esté vacío
+                    if (!string.IsNullOrWhiteSpace(txtTareaNueva.Text))
+                    {
+                        Tarea nuevaTarea = new Tarea
+                        {
+                            Descripcion = txtTareaNueva.Text
+                        };
+
+                        // Agregar la tarea al DataGridView
+                        dataGridViewTareas.Rows.Add(nuevaTarea.Descripcion);
+
+                        List<Tarea> tareas = CargarTareas();
+                        tareas.Add(nuevaTarea);
+
+                        GuardarTareas(tareas);
+
+                        // Limpiar el TextBox después de agregar la tarea
+                        txtTareaNueva.Text = string.Empty;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Por favor, ingresa una tarea antes de agregarla.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+
+                }
+                catch (Exception ex) 
+                {
+
+                }
+
+            }
+
+        }
+
+        /**  Boton Eliminar **/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridViewTareas.SelectedRows.Count > 0)
+                {
+                    // Verifica si el valor de la celda es null antes de convertirlo
+                    var descripcionTarea = dataGridViewTareas.SelectedRows[0].Cells[0].Value;
+
+                    if (descripcionTarea != null) // Si no es null, continuar
+                    {
+                        // Convertir a string solo si el valor no es null
+                        string tarea = descripcionTarea.ToString();
+
+                        // Eliminar la tarea del DataGridView
+                        dataGridViewTareas.Rows.RemoveAt(dataGridViewTareas.SelectedRows[0].Index);
+
+                        // Cargar tareas actuales
+                        List<Tarea> tareas = CargarTareas();
+
+                        // Eliminar la tarea de la lista
+                        tareas.RemoveAll(t => t.Descripcion == tarea);
+
+                        // Guardar las tareas actualizadas en el archivo JSON
+                        GuardarTareas(tareas);
+                    }
+                    else
+                    {
+                        MessageBox.Show("La tarea seleccionada está vacía o no tiene valor.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, selecciona una tarea para eliminar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No puedes borrar algo que no está o ocurrió un error: " + ex.Message, "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+
+       
+    }
+}
